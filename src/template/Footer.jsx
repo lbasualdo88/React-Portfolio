7925ee107913299491867b0a4893ad85e1ab3647
Leonardo Basualdo <lbasualdo88@gmail.com>
2024-08-01 04:15:18 -0300

    update

--- src/template/Footer.jsx
+++ src/template/Footer.jsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <>  
-        <div className="flex flex-col-reverse md:flex-row justify-around items-center mt-6 py-8 bg-color9 dark:bg-dark-color2">
+        <div className="flex flex-col-reverse md:flex-row justify-around items-center py-8 bg-color9 dark:bg-dark-color2">
             <p className="font-Raleway text-color8 text-2xl md:text-3xl dark:text-dark-color8">Copyright © 2024 Leonardo Javier Basualdo</p>
         </div>
     </>
