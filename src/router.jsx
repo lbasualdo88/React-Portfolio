b479389f4f1d4036d4d6247f99cacada5297c0f1
Leonardo Basualdo <lbasualdo88@gmail.com>
2024-08-01 01:57:48 -0300

    update

--- src/router.jsx
+++ src/router.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
                 element: <Readmi />
             },
             {
-                path: '/*',
+                path: '*',
                 element: <Pagina404 />,
             }
         ]       
